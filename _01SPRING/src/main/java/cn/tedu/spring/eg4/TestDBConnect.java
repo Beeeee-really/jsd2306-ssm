63c551bb218123f1eef5f2281818f1e6c04324fc
Beeeee <beeeee2@outlook.com>
2023-08-17 10:26:11 +0800

    Update TestDBConnect.java

--- _01SPRING/src/main/java/cn/tedu/spring/eg4/TestDBConnect.java
+++ _01SPRING/src/main/java/cn/tedu/spring/eg4/TestDBConnect.java
@@ -1,11 +1,10 @@
 package cn.tedu.spring.eg4;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestDBConnect {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("cn.tedu.spring.scope");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("cn.tedu.spring.scope");
         System.out.println("1");
         DBconnect dBconnect1 = context.getBean(DBconnect.class);
         System.out.println(dBconnect1);
