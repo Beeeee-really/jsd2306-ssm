5f1bcb16ca388138d98c8ec776e587bf4f74ac95
Beeeee <beeeee2@outlook.com>
2023-08-17 16:55:06 +0800

    .

--- _02MYBATIS/src/main/java/cn/tedu/_02mybatis/mapper/UserMapper.java
+++ _02MYBATIS/src/main/java/cn/tedu/_02mybatis/mapper/UserMapper.java
@@ -2,8 +2,12 @@ package cn.tedu._02mybatis.mapper;
 
 import cn.tedu._02mybatis.pojo.User;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
-
+/**
+ * Mapper为此接口创建实现类
+ */
+@Mapper
 public interface UserMapper {
     //1.插入一条数据
     @Insert("USE blog;INSERT INTO user(username,password,nickname,created) VALUES(#{username},#{password},#{nickname},#{created});")
--- _02MYBATIS/src/test/java/cn/tedu/_02mybatis/ApplicationTests.java
+++ _02MYBATIS/src/test/java/cn/tedu/_02mybatis/ApplicationTests.java
@@ -1,13 +1,34 @@
 package cn.tedu._02mybatis;
 
+import cn.tedu._02mybatis.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static javax.swing.text.html.HTML.Tag.U;
 
 @SpringBootTest
 class ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+
+
+    /**
+     * 测试方法一定是无参无返回值
+     */
+    @Test
+    void insertTest(){
+        User user = new User();
+        user.setUsername("awa");
+        user.setPassword("111");
+        user.setNickname("222");
+        user.setCreated(new Date());
+
     }
 
 }
