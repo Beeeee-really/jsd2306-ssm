1496cb34cb0fa02665389a5ff7a6681e417a0d5b
Beeeee <beeeee2@outlook.com>
2023-08-21 18:01:05 +0800

    Update

--- _03MVCBOOT01/src/main/java/cn/tedu/_03mvcboot01/controller/UserController.java
+++ _03MVCBOOT01/src/main/java/cn/tedu/_03mvcboot01/controller/UserController.java
@@ -1,5 +1,6 @@
 package cn.tedu._03mvcboot01.controller;
 
+import cn.tedu._03mvcboot01.pojo.dto.UserLoginDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,13 +10,33 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
-
+    /*//方式1
     @RequestMapping("login")
     @ResponseBody//允许以返回值的方式返回给前端
     public String login(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         return "登录成功 userName:" + username + "  password:" + password;
+    }*/
+
+
+    /* //方式2
+    @RequestMapping("login")
+    @ResponseBody
+    public String login(String username, String password) {
+        return username + ":" + password;
+
+    }*/
+
+
+    //3.DTO获取
+    @RequestMapping("login")
+    @ResponseBody
+    public String login(UserLoginDTO userLoginDTO) {
+        String username = userLoginDTO.getUsername();
+        String password = userLoginDTO.getPassword();
+
+        return username + ":" + password;
     }
 
 
