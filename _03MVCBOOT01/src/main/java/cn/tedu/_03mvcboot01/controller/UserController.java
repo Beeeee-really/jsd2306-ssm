dae9f9e301c698eb9421e1fc85ff6453fc7c640d
Beeeee <beeeee2@outlook.com>
2023-08-21 16:56:01 +0800

    .

--- _03MVCBOOT01/src/main/java/cn/tedu/_03mvcboot01/controller/UserController.java
+++ _03MVCBOOT01/src/main/java/cn/tedu/_03mvcboot01/controller/UserController.java
@@ -4,20 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class UserController {
 
     @RequestMapping("login")
     @ResponseBody//允许以返回值的方式返回给前端
-    public String login() {
-        return "登录成功";
+    public String login(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        return "登录成功 userName:" + username + "  password:" + password;
     }
-
 
 
     @RequestMapping("signup")
     @ResponseBody
-    public String signup(){
+    public String signup() {
         return "注册成功";
     }
 }
