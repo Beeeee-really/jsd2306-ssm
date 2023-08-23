f1982da8b02bb654204ec8964bc2113ebbc373af
Beeeee <beeeee2@outlook.com>
2023-08-23 16:24:19 +0800

    Update

--- _04WEIBPO/src/main/java/cn/tedu/_04weibo/controller/UserController.java
+++ _04WEIBPO/src/main/java/cn/tedu/_04weibo/controller/UserController.java
@@ -1,13 +1,15 @@
 package cn.tedu._04weibo.controller;
 
 import cn.tedu._04weibo.mapper.UserMapper;
-import cn.tedu._04weibo.pojo.dto.UserLoginDTO;
 import cn.tedu._04weibo.pojo.dto.UserRegDTO;
 import cn.tedu._04weibo.pojo.entity.User;
 import cn.tedu._04weibo.pojo.vo.UserVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
@@ -18,13 +20,21 @@ public class UserController {
     @Autowired(required = false)
     private UserMapper userMapper;
 
+    /**
+     * 注册功能
+     * @param userRegDTO
+     * @return int
+     * RequestBody注解：将客户端传递过来的JSON格式的数据封装到DTO中
+     */
     @PostMapping("reg")
-    public int reg(UserRegDTO userRegDTO){
+    public int reg(@RequestBody UserRegDTO userRegDTO){
         /*
             1.确认用户名是否被占用 - 查询接口
             2.被占用: 直接返回2
               未被占用: 插入数据[实体类] - 插入接口
          */
+        //soutp + 回车
+        System.out.println("userRegDTO = " + userRegDTO);
         UserVO userVO = userMapper.selectByUsername(userRegDTO.getUsername());
         if (userVO != null) {//用户名被占用
             return 2;
@@ -35,14 +45,6 @@ public class UserController {
         user.setCreated(new Date());
         userMapper.insertUser(user);
         //注册成功
-        return 1;
-    }
-
-
-
-
-    public int login(@RequestBody UserLoginDTO userLoginDTO){
-
         return 1;
     }
 }
