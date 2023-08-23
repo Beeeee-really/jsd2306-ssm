69b9e6efb92f597ecd0d21245dac7b5eee412af4
Beeeee <beeeee2@outlook.com>
2023-08-23 14:44:42 +0800

    Update

---
+++ _04WEIBPO/src/main/java/cn/tedu/_04weibo/pojo/dto/UserRegDTO.java
@@ -0,0 +1,40 @@
+package cn.tedu._04weibo.pojo.dto;
+
+public class UserRegDTO {
+    private String username;
+    private String password;
+    private String nickname;
+
+    @Override
+    public String toString() {
+        return "UserRegDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
