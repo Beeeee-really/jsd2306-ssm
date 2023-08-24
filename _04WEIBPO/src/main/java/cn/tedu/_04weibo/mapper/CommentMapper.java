26824d2b6f0fc9364d26b5e3c9810dd45c7c475f
Beeeee <beeeee2@outlook.com>
2023-08-24 12:51:36 +0800

    Update

--- _04WEIBPO/src/main/java/cn/tedu/_04weibo/mapper/CommentMapper.java
+++ _04WEIBPO/src/main/java/cn/tedu/_04weibo/mapper/CommentMapper.java
@@ -1,4 +1,9 @@
 package cn.tedu._04weibo.mapper;
 
+import cn.tedu._04weibo.pojo.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface CommentMapper {
+    int insert(Comment comment);
 }
